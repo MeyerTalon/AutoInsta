4a23905995b32cb9c69a7d03fe3959a847999ac3
Jacob Goreham <jacobgoreham@gmail.com>
2023-05-24 19:45:53 -0700

    Server package work, empty page loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Matchup from "./pages/Matchup";
+import Vote from "./pages/Vote";
+import NotFound from "./pages/NotFound";
+
 import "./App.css";
 
 function App() {
@@ -18,6 +24,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/matchup" element={<Matchup />} />
+            <Route path="/matchup/:id" element={<Vote />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
