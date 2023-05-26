171d6acdad984ce83e140aa0fce3e6de7f0ef77b
Talon Meyer <talonmeyer12@gmail.com>
2023-05-25 18:50:50 -0700

    Edited server.js, typeDefs.js, auth.js, and  index.js

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');;
+
+const typeDefs = gql`
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        posts: [Post]
+    }
+
+    type Post {
+        _id: ID!
+        caption: String!
+        title: String!
+        image: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input PostInput {
+        _id: ID!
+        caption: String!
+        title: String!
+        image: String!
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String, email: String!, password: String!): Auth
+        savePost(postData: PostInput!): User
+        removeBook(_id: ID!): User
+        editPost(postData: PostInput!): USer
+    }
+`;
+
+module.exports = typeDefs;
