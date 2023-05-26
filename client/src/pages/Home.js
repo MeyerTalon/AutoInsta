19fe5980495a826d16095000ff51e59d99a9113e
Jacob Goreham <jacobgoreham@gmail.com>
2023-05-25 19:46:48 -0700

    Signup and Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,45 +1,35 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+import React from "react";
 
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
-
-  const matchupList = data?.matchups || [];
-
+function Home() {
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
+    <>
+      <div className="container">
+        <div className="row justify-content-center mt-4">
+          <div className="col s12 m6 offset-m3">
+            <div className="card">
+              <div className="card-content">
+                <h5 className="card-title">Home Page</h5>
+                <p>Welcome to AutoInsta!</p>
+                <p>
+                  <strong>UserName</strong>
+                </p>
+                <div className="center">
+                  {/* <img
+                    className="circle"
+                    src="user-image.jpg"
+                    alt="User Image"
+                    width="150"
+                    height="150"
+                  /> */}
+                </div>
+                <p>User Bio</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
-};
+}
 
 export default Home;
