f281941e7080e87cb14be00a385123a25e729722
Jacob Goreham <jacobgoreham@gmail.com>
2023-05-31 19:48:04 -0700

    Signup page edits...

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function Signup() {
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [validated, setValidated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({ username: "", email: "", password: "" });
+  };
+
   return (
     <>
       <div className="container">
@@ -10,41 +53,78 @@ function Signup() {
               <div className="card-content">
                 <span className="card-title">Signup</span>
                 <div className="row">
-                  <form className="col s12">
+                  <Form
+                    noValidate
+                    validated={validated}
+                    onSubmit={handleFormSubmit}
+                  >
                     <div className="row">
                       <div className="input-field col s12">
-                        <input id="username" type="text" className="validate" />
-                        <label htmlFor="username">Username</label>
+                        <Form.Control
+                          id="username"
+                          type="text"
+                          name="username"
+                          value={userFormData.username}
+                          onChange={handleInputChange}
+                          required
+                        />
+                        <Form.Label htmlFor="username">Username</Form.Label>
+                        <Form.Control.Feedback type="invalid">
+                          Username is required!
+                        </Form.Control.Feedback>
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12">
-                        <input id="email" type="email" className="validate" />
-                        <label htmlFor="email">Email</label>
+                        <Form.Control
+                          id="email"
+                          type="email"
+                          name="email"
+                          value={userFormData.email}
+                          onChange={handleInputChange}
+                          required
+                        />
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <Form.Control.Feedback type="invalid">
+                          Email is required!
+                        </Form.Control.Feedback>
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12">
-                        <input
+                        <Form.Control
                           id="password"
                           type="password"
-                          className="validate"
+                          name="password"
+                          value={userFormData.password}
+                          onChange={handleInputChange}
+                          required
                         />
-                        <label htmlFor="password">Password</label>
+                        <Form.Label htmlFor="password">Password</Form.Label>
+                        <Form.Control.Feedback type="invalid">
+                          Password is required!
+                        </Form.Control.Feedback>
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12">
-                        <button
+                        <Button
                           className="btn waves-effect waves-light"
                           type="submit"
                           name="action"
+                          disabled={
+                            !(
+                              userFormData.username &&
+                              userFormData.email &&
+                              userFormData.password
+                            )
+                          }
                         >
                           Signup
-                        </button>
+                        </Button>
                       </div>
                     </div>
-                  </form>
+                  </Form>
                 </div>
               </div>
             </div>
