50bd387df5af39fb40fa0ce50bbbf3ee74c4e72d
Talon Meyer <talonmeyer12@gmail.com>
2023-05-31 20:59:54 -0700

    Added to index.ja in client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri:'/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: "your_graphql_endpoint",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
