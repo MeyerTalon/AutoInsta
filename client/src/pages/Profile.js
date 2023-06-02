fcb95a7a0df75dd8c048a40b3b5a67c4823b0a6d
Jacob <jacobgoreham@gmail.com>
2023-06-02 09:53:20 -0700

    Profile page changes, working on fixing delete button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,55 +1,49 @@
 import React from "react";
 import { removePostId } from "../utils/localStorage";
 import Auth from "../utils/auth";
-import { GET_ME } from "../utils/queries"
-import {useQuery , useMutation} from "@apollo/client"
-import { REMOVE_POST } from '../utils/mutations';
-
+import { GET_ME } from "../utils/queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { REMOVE_POST } from "../utils/mutations";
+import { gql } from "@apollo/client";
 
 function ProfilePage() {
+  const { loading, data } = useQuery(GET_ME);
+  const userData = data?.me || {};
 
-    const {loading, data} = useQuery(GET_ME)
-    const userData = data?.me||{};
+  const [removePost, { error }] = useMutation(REMOVE_POST);
 
-    const [removePost, { error }] = useMutation(REMOVE_POST);
-  const handleDeletePost = (postId) => {
+  const handleDeletePost = async (postId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-   
-
-    const handleDeletePost = async (PostId) => {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-    
-        if (!token) {
-          return false;
-        }
-    
-        try {
-          const { data } = await removePost({
-            variables: { PostId }
-          });
-          // upon success, remove book's id from localStorage
-          removePostId(PostId);
-        } catch (err) {
-          console.error(err);
-        }
-      };
 
     if (!token) {
       return false;
     }
 
     try {
-      // Remove the post's ID from the userData.posts array
-      const updatedPosts = [];
-      for (const post of userData.posts) {
-        if (post.id !== postId) {
-          updatedPosts.push(post);
-        }
-      }
+      const { data } = await removePost({
+        variables: { postId },
+        update(cache, { data }) {
+          // Remove the post from the cache after successful deletion
+          cache.modify({
+            fields: {
+              me(existingMeRef = {}) {
+                const newPostsAfterDeletion = data.removeBook.posts;
 
-      // Update the component state or trigger any necessary update in the parent component
-      // to reflect the updated list of created posts
+                cache.writeFragment({
+                  data: { posts: newPostsAfterDeletion },
+                  fragment: gql`
+                    fragment UpdatedUserPosts on User {
+                      posts
+                    }
+                  `,
+                });
+
+                return existingMeRef;
+              },
+            },
+          });
+        },
+      });
 
       // Remove the post's ID from localStorage
       removePostId(postId);
@@ -59,29 +53,44 @@ function ProfilePage() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h2 style={{ textAlign: "center" }}>My Posts</h2>
-      {userData.posts && userData.posts.length > 0 ? (
-        <div className="posts-container">
-          {userData.posts.map((post, index) => (
-            <div className="post" key={index}>
-              <h3>{post.title}</h3>
-              <p>{post.caption}</p>
-              {post.image && <img src={post.image} alt="Post Image" />}
-              <p>Date: {post.date}</p>
-              <p>Time: {post.time}</p>
-              <p>Interval: {post.interval}</p>
-              <button onClick={() => handleDeletePost(post.id)}>Delete</button>
-              <button className='btn-block btn-danger' onClick={() => handleDeletePost(userData.postId)}>
-                      Delete this Post!
-                    </button>
-              <hr />
+      <style>
+        {`
+          .card-container {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            grid-gap: 20px;
+            justify-items: center;
+          }
+        `}
+      </style>
+      <div className="card-container">
+        {userData.posts && userData.posts.length > 0 ? (
+          userData.posts.map((post) => (
+            <div className="card" key={post._id}>
+              <div className="card-image">
+                {post.imageFile && <img src={post.imageFile} alt="Post Image" />}
+                <span className="card-title">{post.title}</span>
+                <a
+                  className="btn-floating halfway-fab waves-effect waves-light red"
+                  onClick={() => handleDeletePost(post._id)} // Pass postId to handleDeletePost
+                >
+                  <i className="material-icons">X</i>
+                </a>
+              </div>
+              <div className="card-content">
+                <p>{post.caption}</p>
+                <p>Date: {post.date}</p>
+                <p>Time: {post.time}</p>
+                <p>Interval: {post.interval}</p>
+              </div>
             </div>
-          ))}
-        </div>
-      ) : (
-        <p style={{ textAlign: "center" }}>No posts found.</p>
-      )}
+          ))
+        ) : (
+          <p style={{ textAlign: "center" }}>No posts found.</p>
+        )}
+      </div>
     </div>
   );
 }
